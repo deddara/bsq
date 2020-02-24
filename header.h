a98f75bb39800e9c6e520d6ea64d3368f0fe4560
Darla Eddara <deddara@ev-s1.kzn.21-school.ru>
2020-02-24 22:15:59 +0300

    first commit

---
+++ header.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deddara <deddara@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/24 17:58:34 by deddara           #+#    #+#             */
+/*   Updated: 2020/02/24 21:06:54 by deddara          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+#endif
